18ec8deedbe16cc161bdb925bebf1d488c50eab1
paul-clavier <96700160+paul-clavier@users.noreply.github.com>
2023-05-02 19:47:21 +0200

    Create deploy-pages.yml

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import { dependencies } from "./package.json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: "/mugi-blocks/",
     plugins: [
         react(),
         dts({
